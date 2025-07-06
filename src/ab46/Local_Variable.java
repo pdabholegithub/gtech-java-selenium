562adea8dc83f784ea7eb2f762b2ec20e1369789
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-07-06 22:38:32 +0530

    Local Variable & Scanner

---
+++ src/ab46/Local_Variable.java
@@ -0,0 +1,16 @@
+package ab46;
+
+public class Local_Variable {
+
+	public static void main(String[] args) {
+		int a = 100;
+		a = 400;
+		System.out.println(a);
+		int b;
+		b = 90;
+		b = 200;
+		System.out.println(b);
+
+	}
+
+}
--- src/ab46/Scanner_Method.java
+++ src/ab46/Scanner_Method.java
@@ -7,12 +7,12 @@ public class Scanner_Method {
 	public static void main(String[] args) {
 
 		Scanner s1 = new Scanner(System.in);
-
 		System.out.println("Enter your name");
 		String FirstName = s1.next();
 		System.out.println("Are you graduate?");
 		boolean b1 = s1.nextBoolean();
-		byte b2 = s1.nextByte();
+		//byte b2 = s1.nextByte();
+		s1.close();
 
 	}
 
