61010634920dbcb84bc830a2d9eff4de205f6b3c
pdabholegithub <prasad.dabhole@hotmail.com>
2025-07-12 15:19:38 +0530

    pdabholegithub Changes 120725

---
+++ src/ab46/Amazon2.java
@@ -0,0 +1,14 @@
+package ab46;
+
+public class Amazon2 {
+	Amazon2() {
+		System.out.println("My first Constructor");
+	}
+
+	public static void main(String[] args) {
+		Amazon2 a1 = new Amazon2(); //First way to create an object
+		new Amazon2(); //Second way to create an object
+
+	}
+
+}
