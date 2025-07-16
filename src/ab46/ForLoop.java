459d94105071f17d080374e63a34d1369f516d2c
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-07-16 23:20:49 +0530

    ForLoop

---
+++ src/ab46/ForLoop.java
@@ -0,0 +1,13 @@
+package ab46;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<10;i++) 
+		{
+			System.out.println("PraManu");
+		}
+	}
+
+}
---
+++ src/ab46/ForLoop2.java
@@ -0,0 +1,13 @@
+package ab46;
+
+public class ForLoop2 {
+
+	public static void main(String[] args) {
+		 //int i;
+		for(int i=1;i<=10;i++) 
+		{
+			System.out.println(i);
+		}
+	}
+
+}
